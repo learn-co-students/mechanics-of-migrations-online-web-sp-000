2fd34719c3c9fe6099294c27949569ccf6c66cef
Joe Ganobsik <jganobsik@gmail.com>
2019-01-09 15:54:15 +0000

    Done.

---
+++ artist.rb
@@ -0,0 +1,16 @@
+
+
+class CreateArtists < ActiveRecord::Migration
+  def change
+    create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
+    end
+  end
+end
+
+
+class Artist < ActiveRecord::Base
+end
