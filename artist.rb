e5faf7cb5041417d7ce9383ba403cd1f33da1309
Ariel <arielord@buffalo.edu>
2019-03-10 20:36:35 +0000

    Done.

---
+++ artist.rb
@@ -0,0 +1,11 @@
+class Artist < ActiveRecord::Base
+  # attr_accessor :name, :age, :genre, :hometown
+  
+  # def initialize(name: nil, age: nil, genre: nil, hometown: nil)
+  #   @name = name
+  #   @age = age
+  #   @genre = genre
+  #   @hometown = hometown
+  # end
+  
+end
