89441d6aca93be769386458be76de0edf6232979
Connor Cowan <ccowan003@comcast.net>
2019-10-02 16:20:13 -0700

    Done.

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,4 +1,4 @@
-class CreateArtists < ActiveRecord::Migration[4.2]
+class CreateArtists < ActiveRecord::Migration[5.2]
   def change
     create_table :artists do |t|
       t.string :name
@@ -6,5 +6,13 @@ class CreateArtists < ActiveRecord::Migration[4.2]
       t.integer :age
       t.string :hometown
     end
+  end
+  #define the code to execute when the migration is run "do"
+  def up
+
+  end
+  #define the code to execute when the migration is rolled back "undo"
+  def down
+
   end
 end
