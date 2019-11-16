a45df12c7a703979c1e560a27bdaa1627c574774
Blythe Pollard <blytheepollard@gmail.com>
2019-11-16 23:12:04 +0000

    Done.

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -8,7 +8,7 @@ class CreateArtists < ActiveRecord::Migration[5.2]
   def change #primary way of writing migrations
     create_table :artists do |t|
       t.string :name 
-      t.string :integer 
+      t.string :genre 
       t.integer :age 
       t.string :hometown
     end
--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 1) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
-    t.string "integer"
+    t.string "genre"
     t.integer "age"
     t.string "hometown"
   end
