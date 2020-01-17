6298433e5b2d38d9a30a3110ae4a6441bf6ffc50
Michael Carroll <carrollm2@gmail.com>
2020-01-17 13:43:37 +0000

    Done.

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -27,5 +27,11 @@ class CreateArtists < ActiveRecord::Migration[5.2]
   end
 
   def change
-  end  
+    create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
+    end
+  end
 end
