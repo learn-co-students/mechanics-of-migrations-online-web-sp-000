4b90e79df998cf6e437308ddb8135874ce2365f3
joshuaAjack <Mynameisjoshuajack@gmail.com>
2020-04-15 11:02:00 -0700

    Ignor

--- migrate/01_create_artists.rb
+++ migrate/01_create_artists.rb
@@ -1,8 +1,18 @@
  
-class CreateArtists < ActiveRecord::Migration[5.2]
-    def up
+class CreateArtists < ActiveRecord::Migration 
+
+    def up 
     end
-   
-    def down
-    end
-  end
+  
+    def down 
+    end 
+  
+    def change 
+      create_table :artists do |t|
+        t.string :name 
+        t.string :genre 
+        t.integer :age 
+        t.string :hometown 
+      end 
+    end 
+  end  
