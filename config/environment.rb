1249466ffa2a69b335e1bbb27d9622f71dcd708c
ashleymader <ashley.mader12@gmail.com>
2020-04-22 13:24:38 -0400

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -10,5 +10,23 @@ Bundler.require
 
 
 # put the code to connect to the database here
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/artists.sqlite"
+)
 
 require_relative "../artist.rb"
+require_relative "../db/migrate/01_create_artists"
+require_relative "../db/migrate/02_add_favorite_food_to_artists"
+
+# sql = <<-SQL
+# CREATE TABLE IF NOT EXISTS artists (
+# id INTEGER PRIMARY KEY,
+# name TEXT,
+# genre TEXT,
+# age INTEGER,
+# hometown TEXT
+# )
+# SQL
+
+# ActiveRecord::Base.connection.execute(sql)
