9d4a5f0cdb7716fd62a4fdd850ed6bd0b587bb1b
acg2176 <acg2176@columbia.edu>
2020-06-10 23:07:05 -0400

    Done.

---
+++ db/migrate/01_create_artists.rb
@@ -0,0 +1,19 @@
+#migration code
+
+class CreateArtists < ActiveRecord::Migration[5.2]
+    def up
+    end
+   
+    def down
+    end
+
+    #used to write migrations
+    def change
+        create_table :artists do |t|
+            t.string :name
+            t.string :genre
+            t.integer :age
+            t.string :hometown
+        end
+      end
+  end
