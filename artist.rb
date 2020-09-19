c17691b834689c05f9555b32f7fe84344d54d087
khoinguyenkc <khoinguyenkc@gmail.com>
2020-09-19 12:33:44 -0700

    Done.

---
+++ artist.rb
@@ -0,0 +1,6 @@
+class Artist < ActiveRecord::Base
+    #recall the activerecord::base thing gives u a bunch of capability to interact with the sql
+    #u can do things like assign attributes, save it tothe databse, etc..
+    #and class methods like .all....
+    end
+    
