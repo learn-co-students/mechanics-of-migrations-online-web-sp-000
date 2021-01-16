5b9144fa2337189f88a76353bd2b17575128dd99
Chris Houghton <chrishoughton2434@gmail.com>
2021-01-16 15:39:21 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 1) do
   create_table "artists", force: :cascade do |t|
     t.string "name"
     t.string "genre"
-    t.string "age"
+    t.integer "age"
     t.string "hometown"
   end
 
