45b2767e458faf7348450f70e6fdc5268be9f420
Keenah Vancampenhout <kvancamp@mail.com>
2021-01-23 11:48:01 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -9,6 +9,11 @@ require 'bundler/setup'
 Bundler.require
 
 
-# put the code to connect to the database here
+# code to connect to the database
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/artists.sqlite"
+)
+
 
 require_relative "../artist.rb"
