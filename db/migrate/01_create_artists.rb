fefa04f154950820098e01be1ecfd0f79c595103
Jess Pesale <jess_pesale@aol.com>
2021-02-21 21:35:37 -0500

    Done.

---
+++ db/migrate/01_create_artists.rb
@@ -0,0 +1,13 @@
+
+class CreateArtist < ActiveRecord::Migration[5.2]
+
+    def change
+        create_table :artist do |t|
+            t.string :name
+            t.string :genre
+            t.integer :age
+            t.string :hometown
+        end
+    end
+
+end
